7422021781c2a7726a59c210a81e1b67bfa76703
小星尘 <jahui6690429@163.com>
2021-07-08 18:48:09 +0800

    创建基础类

---
+++ service/service_hosp/src/main/java/com/atjh/yygh/mapper/HospitalSetMapper.java
@@ -0,0 +1,9 @@
+package com.atjh.yygh.mapper;
+
+import com.atjh.yygh.model.hosp.HospitalSet;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+
+public interface HospitalSetMapper extends BaseMapper<HospitalSet> {
+
+}
---
+++ service/service_hosp/src/main/java/com/atjh/yygh/service/impl/HospitalSetServiceImpl.java
@@ -0,0 +1,15 @@
+package com.atjh.yygh.service.impl;
+
+
+import com.atjh.yygh.mapper.HospitalSetMapper;
+import com.atjh.yygh.model.hosp.HospitalSet;
+import com.atjh.yygh.service.HospitalSetService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HospitalSetServiceImpl extends ServiceImpl<HospitalSetMapper, HospitalSet> implements HospitalSetService {
+
+
+
+}
